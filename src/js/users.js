00002bc66ec80de748c75e94e314acbaec159e4b
Ernesto Canquiz <caribestic123@gmail.com>
2022-01-24 09:31:12 -0400

    feat: Mocking Modules

---
+++ src/js/users.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+class Users {
+  static all() {
+    return axios.get('/users.json').then(resp => resp.data);
+  }
+}
+
+/*const Users = {
+  all() {
+    return axios.get('/users.json').then(resp => resp.data);
+  }
+}*/
+
+export default Users;
---
+++ test/unit/jest/04-mock-function/mockModules.test.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+import Users from '@/js/users';
+
+//vi.mock('axios');
+
+test('should fetch users', () => {
+  const users = [{name: 'Bob'}];
+  const resp = {data: users};
+  //axios.get.mockResolvedValue(resp);
+  axios.get = vi.fn().mockResolvedValue(resp);
+
+  // or you could use the following depending on your use case:
+  // axios.get = vi.fn().mockImplementation(() => Promise.resolve(resp))
+
+  return Users.all().then(data => expect(data).toEqual(users));
+});
